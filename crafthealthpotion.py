2ca9717667466130326679ba71bd0e8de287f1bf
Jacob Hightower <jah795@nau.edu>
2018-06-20 22:48:33 -0700

    Allowed player to see health. Will come back and add more features when I learn more about python. On to my next project!

--- crafthealthpotion.py
+++ crafthealthpotion.py
@@ -1,7 +1,12 @@
+#importing random function from python
 import random
-
+#default value of health
 health = 50
+
+print("You are wounded and find a health potion. You have 50 out of 100 total hp points. Let's see if you can get to a perfect 100!")
 
 potion_health = random.randint(25,50)
 
 health = health + potion_health
+
+print(health)
